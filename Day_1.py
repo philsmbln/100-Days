4ae5b6fcfe88c69683493abbc2b530ffa5d1c467
0xPhil <37889229+philsmbln@users.noreply.github.com>
2026-05-12 21:03:36 -0700

    Band Name Generator

--- Day_1.py
+++ Day_1.py
@@ -1,5 +1,13 @@
-# Learning Variables
+# Band Name Generator
 
-name = "Yuri"
-l = len(name)
-print(l)
+# Greeting for the Program
+print('Welcome to the Band Name Generator')
+
+# Ask the user where they grew up
+city = input('What\'s the name of the city you grew up in?: ')
+
+# Ask the user for the name of his/her pet
+pet_name = input('What\'s yours pet\'s name?: ')
+
+# Out for the band's name city and pets name combines together
+print('Your band name could be ' + city + " " + pet_name)
