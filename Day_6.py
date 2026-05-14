12c87dfdf49df251cf868dbcb6b174fb0a96d412
0xPhil <37889229+philsmbln@users.noreply.github.com>
2026-05-13 19:52:08 -0700

    Reeborg's World Maze

---
+++ Day_6.py
@@ -0,0 +1,16 @@
+# Reeborg's World Maze
+# Other variables is internally defined
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
