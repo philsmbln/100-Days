5e4697f22b66d6a5f7226824574e62c334c4c96f
0xphil <37889229+philsmbln@users.noreply.github.com>
2026-05-20 21:38:40 -0700

    Hangman Improvements

--- Day_7.py
+++ Day_7.py
@@ -1,14 +1,13 @@
-import random
+import random as rand
 
 from hangman_words import word_list
 from hangman_art import stages, logo
 
-lives = 6
-
 print(logo)
 
-chosen_word = random.choice(word_list)
-print(chosen_word)
+lives = 6
+
+chosen_word = rand.choice(word_list)
 
 placeholder = ""
 word_length = len(chosen_word)
@@ -17,37 +16,42 @@ for position in range(word_length):
 print("Word to guess: " + placeholder)
 
 game_over = False
-correct_letters = []
+guess_letter = []
 
 while not game_over:
 
-    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    print(f'*********** {lives}/6 Lives Left **********')
     guess = input("Guess a letter: ").lower()
-    
-    if guess in correct_letters:
-        print(f"You've already guessed {guess}")
+
+    while len(guess) != 1 or not guess.isalpha():
+        guess = input("Please only choose one letter from (a-z): ").lower()
+
+    if guess in guess_letter:
+        print(f'You\'ve guess letter \'{guess}\' already!')
+    elif guess not in chosen_word:
+        lives -= 1
+        guess_letter.append(guess)
+        print(f'Letter \'{guess}\' is not in the word, you lose a life.')
+    else:
+        guess_letter.append(guess)
 
     display = ""
 
     for letter in chosen_word:
         if letter == guess:
             display += letter
-            correct_letters.append(guess)
-        elif letter in correct_letters:
+            guess_letter.append(guess)
+        elif letter in guess_letter:
             display += letter
         else:
             display += "_"
 
     print("Word to guess: " + display)
 
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"You guessed {guess}, that's not in the word. You lose a life.")
-
-        if lives == 0:
-            game_over = True
-
-            print(f"***********************IT WAS {chosen_word}! YOU LOSE**********************")
+    if lives == 0:
+        game_over = True
+        print(f'Correct word: {chosen_word}')
+        print(f"You Lose, Game Over")
 
     if "_" not in display:
         game_over = True
