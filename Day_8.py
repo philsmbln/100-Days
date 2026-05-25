488e63e12f17c9aa7c8e61237ddcd29483e14192
0xphil <37889229+philsmbln@users.noreply.github.com>
2026-05-25 07:42:31 -0700

    Caesar Cipher

--- Day_8.py
+++ Day_8.py
@@ -1,8 +1,26 @@
-def greet(name, greeting):
-    print(f"{greeting} {name}")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-greet("Angela", "Hello")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}")
+def caesar(original_text, shift_amount):
+    coded_message = ""
+
+    for letter in original_text:
+        if direction == "encode":
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            coded_message += alphabet[shifted_position]
+
+        elif direction == "decode":
+            shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            coded_message += alphabet[shifted_position]
+
+    print(f"Here is the encoded result: {coded_message}")
+
+caesar(original_text=text, shift_amount=shift)
+
+
+
