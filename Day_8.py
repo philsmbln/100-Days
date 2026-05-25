0bf7ec62ce216b1f1a5481346fc1140fea94681f
0xphil <37889229+philsmbln@users.noreply.github.com>
2026-05-24 21:16:08 -0700

    Practice

---
+++ Day_8.py
@@ -0,0 +1,8 @@
+def greet(name, greeting):
+    print(f"{greeting} {name}")
+
+greet("Angela", "Hello")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
